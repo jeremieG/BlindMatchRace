ddb8753303c4c2a35bad4d5c736f6a64608331d5
jeremieG <jeremiegabay@gmail.com>
2014-05-04 22:21:52 +0300

    API-Key refreshed
    Still needs to finish some UI modifications (buoy buttons, removable
    markers…)

--- BlindMatchRace/src/com/blindmatchrace/classes/GetBuoysTask.java
+++ BlindMatchRace/src/com/blindmatchrace/classes/GetBuoysTask.java
@@ -17,7 +17,6 @@ import com.blindmatchrace.modules.JsonReader;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
